2f6e5cd6e928179b7859065e6d5f2f9b4256d002
沙滩上的鱼 <3516464843@qq.com>
2024-01-23 13:47:39 +0800

    feat: add Card Compnent

---
+++ src/components/Card/Text.vue
@@ -0,0 +1,42 @@
+<template>
+  <div style="padding: 14px">
+    <span>{{ props.title }}</span>
+    <div class="bottom">
+      <ElRow v-for="{ value, key } in props.info">
+        <ElText size="small">{{ key }}：{{ value }}</ElText>
+      </ElRow>
+      <template v-if="props.detailID !== 0">
+        <ElRow>
+          <ElButton type="primary" size="small" link @click="router.push(goto)">查看详情</ElButton>
+        </ElRow>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import router from "@/router";
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  info: {
+    type: Object,
+    default: () => {
+      return {}
+    }
+  },
+  detailId: {
+    type: Number,
+    default: 0
+  },
+  pageRoute: {
+    type: String,
+    default: ''
+  }
+});
+
+const goto: string = `/admin/${props.pageRoute}/detail/${props.detailId}`;
+</script>
--- src/components/Card/index.vue
+++ src/components/Card/index.vue
@@ -1,10 +1,46 @@
 <template>
-  <ElCard :body-style="{ padding: '2px' }">
-
+  <ElCard :body-style="{ padding: '0px' }">
+    <template v-if="props.cardType === 'book'">
+      <img :src="props.image" class="image" />
+      <CardText
+        :title="props.title"
+        :info="props.info"
+        :detail-id="props.detailId"
+        :page-route="props.pageRoute"
+      />
+    </template>
+    <template v-else-if="props.cardType === 'payment'">
+      <ElRow justify="center">
+        <ElAvatar style="margin-top: 12px" shape="circle" size="large" :src="props.image" />
+      </ElRow>
+      <CardText
+        :title="props.title"
+        :info="props.info"
+        :detail-id="props.detailId"
+        :page-route="props.pageRoute"
+      />
+    </template>
+    <template v-else-if="props.cardType === 'activity'">
+      <ElRow>
+        <ElCol :span="8">
+          <img style="margin: 6px" :src="props.image" class="image" />
+        </ElCol>
+        <ElCol :span="16">
+          <CardText
+            :title="props.title"
+            :info="props.info"
+            :detail-id="props.detailId"
+            :page-route="props.pageRoute"
+          />
+        </ElCol>
+      </ElRow>
+    </template>
   </ElCard>
 </template>
 
 <script lang="ts" setup>
+import CardText from "@/components/Card/Text.vue";
+
 const props = defineProps({
   image: {
     type: String,
@@ -20,5 +56,17 @@ const props = defineProps({
       return {}
     }
   },
+  detailId: {
+    type: Number,
+    default: 0
+  },
+  pageRoute: {
+    type: String,
+    default: ''
+  },
+  cardType: {
+    type: String,
+    default: ''
+  }
 });
 </script>
