9ab73ccf6fd5e981eb2e4cbf82ace1d79433a07e
kenlig <28685375+kenlig@users.noreply.github.com>
2024-01-25 20:10:19 +0800

    feat:finish login and findpassword pages

--- src/views/common/login/index.vue
+++ src/views/common/login/index.vue
@@ -14,17 +14,17 @@
                         <el-button link @click="toFindPassword">忘记密码?</el-button>
                     </div>
                     <div>
-                        <el-button type="primary" class="input" @click="login">登录</el-button>
+                        <el-button type="primary" class="input" @click="submit">登录</el-button>
                     </div>
                 </el-tab-pane>
                 <el-tab-pane label="找回密码">
                     <div>
-                        <el-input v-model="phone" placeholder="邮箱" class="input" />
+                        <el-input v-model="username" placeholder="用户名" class="input" />
                     </div>
-                    <div>
+                    <!-- <div>
                         <el-input v-model="code" placeholder="验证码" style="width: 50%; margin: 4%;" />
                         <el-button class="inputcodebutton" type="primary" @click="getVerificationCode">发送验证码</el-button>
-                    </div>
+                    </div> -->
                     <div>
                         <el-input v-model="password" type="password" placeholder="密码" class="input" />
                     </div>
@@ -32,7 +32,7 @@
                         <el-input v-model="repeatPassword" type="password" placeholder="重复密码" class="input" />
                     </div>
                     <div>
-                        <el-button type="primary" class="input" @click="register">找回密码</el-button>
+                        <el-button type="primary" class="input" @click="submitFindPwd">找回密码</el-button>
                     </div>
                 </el-tab-pane>
             </el-tabs>
@@ -40,9 +40,61 @@
     </div>
 </template>
 <script setup>
-import { checkEmail, checkPhone } from '@/utils.js';
+import { checkEmail, checkPhone } from '@/utils.js'
 import { ElMessage } from 'element-plus'
 import { ref } from 'vue'
+import { login } from '@/api';
+const username = ref('')
+const password = ref('')
+const repeatPassword=ref('')
+const router = useRouter();
+const submit = async () => {
+    if (
+        username.value === null ||
+        username.value === undefined ||
+        username.value === "" ||
+        password.value === "" ||
+        password.value === null ||
+        password.value === undefined
+    ) {
+        ElMessage.error("用户名或密码为空");
+    } else {
+        const json = { username: username.value, password: password.value };
+        const { data } = await login.login(json)
+        if (data.code === 0) {
+            localStorage.setItem("token", data.data.token);
+            await router.push("/admin/home");
+            ElMessage.success("登陆成功");
+        }
+        else {
+            ElMessage.error('登陆失败！请联系系统管理员！');
+        }
+    }
+}
+
+const submitFindPwd = async () => {
+    if (
+        username.value === null ||
+        username.value === undefined ||
+        username.value === "" ||
+        password.value === "" ||
+        password.value === null ||
+        password.value === undefined
+    ) {
+        ElMessage.error("用户名或密码为空");
+    } else {
+        const json = { username: username.value, password: password.value };
+        const { data } = await login.findPassword(json)
+        if (data.code === 0) {
+            ElMessage.success("修改成功");
+            await router.push('/login')
+        }
+        else {
+            ElMessage.error('修改失败！请联系系统管理员！');
+        }
+    }
+}
+
 </script>
 <style>
 * {
@@ -53,7 +105,7 @@ import { ref } from 'vue'
 .container {
     background-color: #F0F2F5;
     height: 100vh;
-    width: 100vw;
+    width: 100%;
     margin: 0px;
     display: flex;
     justify-content: center;
