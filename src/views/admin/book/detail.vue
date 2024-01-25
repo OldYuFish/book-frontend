ee3766c6ac1ac35ec166a555db102fe1ba0c6a30
kenlig <28685375+kenlig@users.noreply.github.com>
2024-01-25 21:29:40 +0800

    feat:add book detail page

--- src/views/admin/book/detail.vue
+++ src/views/admin/book/detail.vue
@@ -10,9 +10,9 @@
                         <el-col :span="12">
                             <div style="font-size: 35px;">{{ bookName }}</div>
                         </el-col>
-                        <el-col :span="12">
+                        <!-- <el-col :span="12">
                             <el-button type="primary">立即预订</el-button>
-                        </el-col>
+                        </el-col> -->
                     </el-row>
                     <el-row>
                         <el-button type="primary">50万+人次借阅</el-button>
@@ -21,8 +21,8 @@
                         <el-col :span="4">
                             更新时间
                         </el-col>
-                        <el-col :span="8">
-                            2022-11-15 09:31:45
+                        <el-col :span="4">
+                            {{ data.data.bookInformation.updateTime }}
                         </el-col>
                     </el-row>
                     <el-row>
@@ -30,19 +30,19 @@
                             主编作者
                         </el-col>
                         <el-col :span="4">
-                            XXX
+                            {{ data.data.bookInformation.author }}
                         </el-col>
                         <el-col :span="4">
                             出版日期
                         </el-col>
                         <el-col :span="4">
-                            2022-01-17
+                            {{ data.data.bookInformation.publicDate }}
                         </el-col>
                         <el-col :span="4">
                             出版社
                         </el-col>
                         <el-col :span="4">
-                            北京大学出版社
+                            {{ data.data.bookInformation.press }}
                         </el-col>
                     </el-row>
                     <el-row>
@@ -50,13 +50,13 @@
                             类型
                         </el-col>
                         <el-col :span="4">
-                            工程学 > 计算机类
+                            {{ data.data.bookInformation.type }}
                         </el-col>
                         <el-col :span="4">
                             所示分类
                         </el-col>
                         <el-col :span="4">
-                            技术类
+                            {{ data.data.bookInformation.category }}
                         </el-col>
                         <el-col :span="4">
                             ISBN/ISSN
@@ -70,19 +70,19 @@
                             图书状态
                         </el-col>
                         <el-col :span="4">
-                            已上架
+                            {{ data.data.bookInformation.status }}
                         </el-col>
                         <el-col :span="4">
                             图书册数
                         </el-col>
                         <el-col :span="4">
-                            20
+                            {{ data.data.bookInformation.number }}
                         </el-col>
                         <el-col :span="4">
                             已借出
                         </el-col>
                         <el-col :span="4">
-                            14
+                            {{ data.data.bookInformation.borrowNumber }}
                         </el-col>
                     </el-row>
                 </el-col>
@@ -90,8 +90,8 @@
         </el-header>
         <el-main>
             <el-tabs>
-                <el-tab-pane label="图书简介" name="first">图书简介内容</el-tab-pane>
-                <el-tab-pane label="读者目录" name="second">读者目录内容</el-tab-pane>
+                <el-tab-pane label="图书简介" name="first">{{ data.data.bookInformation.description }}</el-tab-pane>
+                <el-tab-pane label="读者目录" name="second">{{ chapter }}</el-tab-pane>
                 <el-tab-pane label="读者评价" name="third">读者评价内容</el-tab-pane>
             </el-tabs>
             <el-card class="box-card">
@@ -101,8 +101,40 @@
 </template>
 
 <script lang="ts" setup>
-const fits = ['fill', 'contain', 'cover', 'none', 'scale-down']
-let url = 'https://fecdn.luogu.com.cn/luogu/logo.png?0fdd294ff62e331d2f70e1a37ba4ee02'
+import { bookManage } from '@/api';
+import { ElMessage } from 'element-plus';
 
-let bookName = '111'
+const fits = ['fill', 'contain', 'cover', 'none', 'scale-down']
+let url = ref('https://fecdn.luogu.com.cn/luogu/logo.png?0fdd294ff62e331d2f70e1a37ba4ee02')
+let data=ref()
+let chapter=ref('')
+const route = useRoute()
+
+const getBookInfo = async () => {
+    const json = { id: route.params.aid }
+    const { Data } = await bookManage.info(json)
+    if (Data.code === 0) {
+        data=Data
+        // data.data.bookInformation.id
+        url=data.data.bookInformation.image
+        // data.data.bookInformation.title
+        // data.data.bookInformation.category
+        // data.data.bookInformation.type
+        // data.data.bookInformation.author
+        // data.data.bookInformation.press
+        // data.data.bookInformation.publicDate
+        // data.data.bookInformation.borrowTimes
+        // data.data.bookInformation.borrowNumber
+        // data.data.bookInformation.number
+        // data.data.bookInformation.status
+        // data.data.bookInformation.updateTime
+        // data.data.bookInformation.description
+        for (let i = 0; i < data.data.bookInformation.directory.length; i++) {
+            chapter+=data.data.bookInformation.directory[i].chapterNumber+'  '+data.data.bookInformation.directory[i].sectionNumber+'  '+data.data.bookInformation.directory[i].chapterName
+        }
+    else {
+        ElMessage.error("查询书籍详情信息失败!")
+    }
+}
+getBookInfo()
 </script>
